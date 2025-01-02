4b12cc7b8861bbc81fb90fdd0311ac5f95238115
DIOGO\diogo <diogo_paes@eticalgarve.com>
2025-01-02 21:17:36 +0000

    ???

--- Shaped o Oficial/Assets/Scripts/Player.cs
+++ Shaped o Oficial/Assets/Scripts/Player.cs
@@ -1,52 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Header("Movement")]
-    [SerializeField] private float _gravityForce;
-    [SerializeField] private float _playerGravity;
-    [SerializeField] private float _fallGravityMult;
-    [SerializeField] private float _maxFallSpeed;
-    [SerializeField] private float _fastFallGravityMult;
-    [SerializeField] private float _maxFastFallSpeed;
+    [SerializeField] private float _speed;
+    //[SerializeField] private float _groundDistance;
 
-    [Header("Run")]
-    [SerializeField] private float _runMaxSpeed;
-    [SerializeField] private float _runAcceleration;
-    [SerializeField] private float _runAccelAmount;
-    [SerializeField] private float _runDecceleration;
-    [SerializeField] private float _runDeccelAmount;
-    [SerializeField] private float _accelInAir;
-    [SerializeField] private float _deccelInAir;
-    [SerializeField] private bool _doConserveMomentum = true;
+    //[SerializeField] private LayerMask _terrainLayer;
+    private Rigidbody _rb;
 
-    [Header("Jump")]
-    [SerializeField] private float _jumpHeight;
-    [SerializeField] private float _jumpTimeToApex;
-    [SerializeField] private float _jumpForce;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
 
-    [Header("Both Jumps")]
-    [SerializeField] private float _jumpCutGravityMult;
-    [SerializeField] private float _jumpHangGravityMult;
-    [SerializeField] private float _jumpHangTimeThreshold;
-    [SerializeField] private float _jumpHangAccelerationMult;
-    [SerializeField] private float _jumpHangMaxSpeedMult;
+    private void Update()
+    {
+        Movement();
+    }
 
-    [Header("Wall Jump")]
-    [SerializeField] private Vector3 _wallJumpForce;
-    [SerializeField] private float _wallJumpRunLerp;
-    [SerializeField] private float _wallJumpTime;
-    [SerializeField] private bool _doTurnOnWallJump;
-
-    [Header("Slide")]
-    [SerializeField] float _slideSpeed;
-    [SerializeField] float _slideAccel;
-
-    [Header("Assists")]
-    [SerializeField] private float _coyoteTime;
-    [SerializeField] private float _JumpInputBUfferTime;
+    private void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 moveDir = new Vector3(x, 0, y);
+        _rb.velocity = moveDir * _speed;
+    }
 
 
 }
